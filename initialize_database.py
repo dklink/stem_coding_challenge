d3efa1a6b4c8877316dece5e1e08385f282a4473
dklink <dougdklink3@gmail.com>
2023-05-07 14:35:06 -0700

    finally got db initialization to work properly

---
+++ initialize_database.py
@@ -0,0 +1,4 @@
+"""External script to enter into moods_app.init_db so that package imports behave properly."""
+from moods_app import init_db
+
+init_db.init_db()
--- moods_app/init_db.py
+++ moods_app/init_db.py
@@ -1,7 +1,13 @@
-"""initializes a new sqlite database using sqlalchemy"""
+"""tool to initialize a new sqlite db from our model definitions"""
 
-from resources.mood_capture import *
+from moods_app.resources.base import Base
+from pathlib import Path
+import moods_app.resources.mood_capture
+import moods_app.resources.user
+
 from sqlalchemy import create_engine
 
-engine = create_engine("sqlite:///database/sqlite.db")  #TODO put this path in a env file
-Base.metadata.create_all(engine)
+def init_db():
+    db_path = Path(__file__).parent / "database" / "sqlite.db"
+    engine = create_engine("sqlite:///" + str(db_path))  #TODO put this path in a env file
+    Base.metadata.create_all(engine)

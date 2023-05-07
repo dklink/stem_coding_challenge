f0902fc6d5b1d6f30a5541df753313371eb42347
dklink <dougdklink3@gmail.com>
2023-05-06 18:48:25 -0700

    added some todo notes

--- moods_app/init_db.py
+++ moods_app/init_db.py
@@ -3,5 +3,5 @@
 from resources.mood_capture import *
 from sqlalchemy import create_engine
 
-engine = create_engine("sqlite:///database.db", echo=True)
+engine = create_engine("sqlite:///database.db")  #TODO put this path in a env file
 Base.metadata.create_all(engine)

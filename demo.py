89edc06f11b0ec8f0c62e9c38392cdda3000845f
dklink <dougdklink3@gmail.com>
2023-05-09 14:00:20 -0700

    finished the demo

--- demo.py
+++ demo.py
@@ -1,76 +1,96 @@
+"""
+This script runs a demo of hitting the api endpoints a few different ways.
+To launch the api, run `flask --app api run` in the "moods_app" directory
+"""
 import requests
 
-base = "http://127.0.0.1:5000"
+if __name__ == "__main__":
+    base_url = "http://127.0.0.1:5000"  # default flask port
 
+    print("First, we'll create a new user, with POST /users.  Here's the response:")
+    user_response = requests.post(base_url + "/users")
+    assert user_response.status_code == 200
+    print(f"\tStatus code: {user_response.status_code}; Response: {user_response.json()}")
+    print("Put that api key somewhere safe, you'll need it to authenticate the other endpoints!")
+    user1 = user_response.json()
+    print()
 
-print("First, we'll create a new user, with POST /users.")
-response1 = requests.post(base + "/users")
-assert response1.status_code == 200
-print(f"We got back HTTP code 200.  Excellent!")
-user1 = response1.json()
-print(f"Our new user has ID {user1['user_id']} and api key {user1['api_key']}.  Dont tell anybody, though!")
-print()
-
-print(f"Now, we'll add some mood captures for user {user1['user_id']}")
-r1 = requests.post(
-    base + "/mood-captures",
-    data={
-            "user_id": user1["user_id"],
-            "longitude": -122.4,
-            "latitude": 37.8,
-            "mood": "sad",
-        },
-    headers={"x-api-key": user1["api_key"]}
-)
-r2 = requests.post(
-    base + "/mood-captures",
-    data={
-            "user_id": user1["user_id"],
-            "longitude": -130.5,
-            "latitude": 90,
-            "mood": "happy",
-        },
+    print(f"Now, we'll add some mood captures for user {user1['user_id']} using POST /mood-captures")
+    r1 = requests.post(
+        base_url + "/mood-captures",
+        data={
+                "user_id": user1["user_id"],
+                "longitude": 10.3,
+                "latitude": 15.2,
+                "mood": "sad",
+            },
         headers={"x-api-key": user1["api_key"]}
     )
-r3 = requests.post(
-    base + "/mood-captures",
-    data={
-            "user_id": user1["user_id"],
-            "longitude": -120.2,
-            "latitude": -45,
-            "mood": "happy",
-        },
-        headers={"x-api-key": user1["api_key"]}
-    )
-r4 = requests.post(
-    base + "/mood-captures",
-    data={
-            "user_id": user1["user_id"],
-            "longitude": -122.4,
-            "latitude": 37.8,
-            "mood": "happy",
-        },
-        headers={"x-api-key": user1["api_key"]}
-    )
-print("And here's what we got back:")
-for response in [r1, r2, r3, r4]:
-    print(f"Status code: {response.status_code}; Response: {response.json()}")
-print()
+    r2 = requests.post(
+        base_url + "/mood-captures",
+        data={
+                "user_id": user1["user_id"],
+                "longitude": 0.1,
+                "latitude": 16.2,
+                "mood": "happy",
+            },
+            headers={"x-api-key": user1["api_key"]}
+        )
+    r3 = requests.post(
+        base_url + "/mood-captures",
+        data={
+                "user_id": user1["user_id"],
+                "longitude": 0,
+                "latitude": 14,
+                "mood": "happy",
+            },
+            headers={"x-api-key": user1["api_key"]}
+        )
+    r4 = requests.post(
+        base_url + "/mood-captures",
+        data={
+                "user_id": user1["user_id"],
+                "longitude": -10,
+                "latitude": 15.2,
+                "mood": "happy",
+            },
+            headers={"x-api-key": user1["api_key"]}
+        )
+    print("And here's what we got back:")
+    for response in [r1, r2, r3, r4]:
+        print(f"\tStatus code: {response.status_code}; Response: {response.json()}")
+    print()
 
-print(f"Now, let's get the distribution of user {user1['user_id']}'s moods:")
-r5 = requests.get(
-    base + f"/mood-captures/frequen cy-distribution?user_id={user1['user_id']}",
-)
-print(r5.status_code)
-assert r5.status_code == 401
-print(f"Status code: {r5.status_code}; Response: {r5.text}")
-print("Oops, we forgot to include the user's api key!  Let's try again.")
-r6 = requests.get(
-    base + f"/mood-captures/frequency-distribution?user_id={user1['user_id']}",
-    headers={"x-api-key": user1["api_key"]}
-)
-print(f"Status code: {r5.status_code}; Response: {r5.json()}")
-print("And finally, ")
-'''
-r7 = requests.get(base + "/mood-captures/nearest-happy?user_id=123&latitude=0&longitude=-120")
-'''
+    print(f"Now, let's get the distribution of user {user1['user_id']}'s moods using GET /mood-captures/frequency-distribution")
+    r5 = requests.get(
+        base_url + f"/mood-captures/frequency-distribution?user_id={user1['user_id']}",
+    )
+    assert r5.status_code == 401
+    print(f"\tStatus code: {r5.status_code}; Response: {r5.text}")
+    print("Oops, we forgot to include the user's api key in the headers!  Let's try again.")
+    r6 = requests.get(
+        base_url + f"/mood-captures/frequency-distribution?user_id={user1['user_id']}",
+        headers={"x-api-key": user1["api_key"]}
+    )
+    print(f"\tStatus code: {r6.status_code}; Response: {r6.json()}")
+    print()
+    target_lat=20
+    target_lon=0
+    print(f"Finally, let's find the user's nearest location to {target_lat} latitude, {target_lon} longitude.")
+    print("We'll use GET /mood-captures/nearest-happy to do this.")
+    print("Hint: the answer should be (16.2, 0.1).")
+    r7 = requests.get(
+        base_url + f"/mood-captures/nearest-happy?user_id={user1['user_id']}",
+        headers={"x-api-key": user1["api_key"]},
+    )
+    assert r7.status_code == 400
+    print(f"\tStatus code: {r7.status_code}; Response: {r7.text}")
+    print("Oops, we forgot to include the target location!  Let's try again.")
+    r8 = requests.get(
+        base_url + f"/mood-captures/nearest-happy?user_id={user1['user_id']}&latitude={target_lat}&longitude={target_lon}",
+        headers={"x-api-key": user1["api_key"]},
+    )
+    print(f"\tStatus code: {r8.status_code}; Response: {r8.json()}")
+    print()
+
+    print("That's all, folks!")

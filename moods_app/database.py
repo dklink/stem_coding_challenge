9077f9e4d13a6a7f33ff29848c5509454b957af9
dklink <dougdklink3@gmail.com>
2023-05-09 14:00:14 -0700

    made the database code safe to wherever the flask app is run from

--- moods_app/database.py
+++ moods_app/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import dotenv_values
+from pathlib import Path
 
 
 def get_filesystem_db_engine():
-    config = dotenv_values(".env")
-    return create_engine(f"sqlite:///{config['SQLITE_DATABASE_PATH']}")  # filesystem db
+    config = dotenv_values(Path(__file__).parent / ".env")
+    db_path = (Path(__file__).parent / config['SQLITE_DATABASE_PATH']).absolute()
+    return create_engine(f"sqlite:////{db_path}")  # filesystem db
 
 def get_scoped_session(engine):
     return scoped_session(sessionmaker(autocommit=False,
